20f494f5b1c3d57cf4a9b85283462c29fdf85849
Naseeh A N <106654509+naseehan@users.noreply.github.com>
2025-07-01 23:59:00 +0530

    update html,css,js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let isMenuOpen = false;
 let mobNav = document.querySelector('.mobile-nav');
 let hamClass = document.querySelector('.hamburger')
+
+
+
 hamClass.addEventListener('click', function() {
-
 mobNav.classList.toggle('mobile-nav-toggle');
-
 })
